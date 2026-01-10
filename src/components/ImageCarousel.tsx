b56ef57bf5f227f667d0597c6d0fdf591cb967d1
slavikbard <83838093+slavikbard@users.noreply.github.com>
2026-01-10 10:58:30 +0200

    Updated ImageCarousel.tsx

--- src/components/ImageCarousel.tsx
+++ src/components/ImageCarousel.tsx
@@ -8,6 +8,7 @@ const ImageCarousel = () => {
     'https://images.pexels.com/photos/4792509/pexels-photo-4792509.jpeg?auto=compress&cs=tinysrgb&w=1920',
     'https://images.pexels.com/photos/6474471/pexels-photo-6474471.jpeg?auto=compress&cs=tinysrgb&w=1920',
     'https://images.pexels.com/photos/4792284/pexels-photo-4792284.jpeg?auto=compress&cs=tinysrgb&w=1920',
+    'https://i.ibb.co/nqn0WzDX/blog1.jpg',
   ];
 
   const [currentIndex, setCurrentIndex] = useState(0);
