92105fe41c3aaf2396f2616275407bde8778bf80
slavikbard <83838093+slavikbard@users.noreply.github.com>
2026-01-11 00:59:03 +0200

    Updated ImageCarousel.tsx

--- src/components/ImageCarousel.tsx
+++ src/components/ImageCarousel.tsx
@@ -5,7 +5,8 @@ const ImageCarousel = () => {
   const images = [
     'https://images.pexels.com/photos/1669754/pexels-photo-1669754.jpeg?auto=compress&cs=tinysrgb&w=1920',
     'https://i.ibb.co/9mTRK6MB/bimteam-4-optimized.jpg',
-  
+   'https://i.ibb.co/jkxGpN2r/privatehomeomer-17.jpg',
+    
     'https://i.ibb.co/0RbKwyk9/penthouse-3.jpg',
     'https://i.ibb.co/39C4NcF7/blog2.jpg',
     'https://i.ibb.co/przqrMQP/blog3.jpg',
